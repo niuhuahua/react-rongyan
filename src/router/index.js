18cbee012c87343d74f1ccc1cd12548acb884de8
niu hua hua <1679392431@qq.com>
2018-08-24 15:10:59 +0800

    change

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,6 @@ import asyncComponent from '../utils/asyncComponent';
 
 import app from '../page/index/index'
 const login = asyncComponent(()=>import('../page/login/login'))
-const bodyView = asyncComponent(()=>import('../page/bodyView'))
-const listView = asyncComponent(()=>import('../page/listView'))
 const proList = asyncComponent(()=>import('../page/index/proList'))
 const proDetail = asyncComponent(()=>import('../page/proDetail/proDetail'))
 export default class RouteConfig extends Component {
@@ -14,10 +12,9 @@ export default class RouteConfig extends Component {
         return(
          <BrowserRouter>
             <Switch>
+              <Route path="/" exact component={app}></Route>
               <Route path="/index" exact component={app}></Route>
               <Route path="/login" component={login}></Route>
-              <Route path="/body" component={bodyView}></Route>
-              <Route path="/list" component={listView}></Route>
               <Route path='/index/:type' component={proList} />
               <Route path='/prodetail' component={proDetail}></Route>
               <Redirect to="/"></Redirect>
